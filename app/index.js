ec94d4e08fe535609eeb343635aba0bfb2721829
Pakita01 <pakitastudio@gmail.com>
2024-11-25 12:08:51 -0400

    cambio

--- app/index.js
+++ app/index.js
@@ -65,8 +65,12 @@ const ContactsModuleInit = () => {
           <input class="contacts-list-item-phone-input" type="text" value="${contact.phone}" readonly>
         </div>
         <div class="btns-container">
-          <button class="edit-btn">...</button>
-          <button class="delete-btn">...</button>
+          <button class="edit-btn"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+            </svg></button>
+          <button class="delete-btn"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+              <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z" clip-rule="evenodd" />
+            </svg></button>
         </div>
       `;
       list.append(li);
@@ -170,6 +174,12 @@ list.addEventListener('click', e => {
 
     if (li.classList.contains('editando')) {
       
+      const nameIsValid = NAME_REGEX.test(nameEditInput.value);
+        if (!nameIsValid) {
+          alert('La primera letra de cada nombre debe ser en Mayuscula.');
+          return;
+        }
+
       const phoneIsValid = PHONE_REGEX.test(phoneEditInput.value);
         if (!phoneIsValid) {
           alert('El número de teléfono debe ser un numero venezolano.');
